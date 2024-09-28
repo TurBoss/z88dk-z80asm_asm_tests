0f2ff530e1b9e15941b881eaa66ad83ef519f7d7
TurBoss <j.l.toledano.l@gmail.com>
2024-09-28 02:30:20 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,49 +1,26 @@
 #include "ez80f92.inc"
 
 
-	.ASSUME	ADL = 1
+	ASSUME	ADL = 1
 
-    ORG     0x00000
+    ORG     0x40000
 
-	JP		_MOS_INIT
-
-	ALIGN	64			; The executable header is from byte 64 onwards
-
-	DB		"MOS"		; Flag for MOS - to confirm this is LD valid MOS command
-	DB		0x00		; MOS header version 0
-	DB		0x01		; Flag for run mode (0: Z80, 1: ADL)
+	call	_INIT
 
 
-_MOS_INIT:
+_INIT:
 
-	LD 		SP,	0xBFFFF	; Initialize the stack pointer
-
-	PUSH	AF			; Preserve registers
-	PUSH	BC
-	PUSH	DE
-	PUSH	IX
-	PUSH	IY			; Need to preserve IY for MOS
-
-	LD		A,	MB		; Save MB
-	PUSH 	AF
-	XOR 	A
-	LD 		MB,	A		; Clear to zero so MOS API calls know how to use 24-bit addresses.
+    LD		SP,	0BFFFFh
 
 	CALL	_MAIN
 
-	POP 	AF
-	LD		MB,	A
-
-	POP		IY			; Restore registers
-	POP		IX
-	POP		DE
-	POP 	BC
-	POP		AF
 	RET
 
 
 _MAIN:
-	;DI					; disable interrupts
+	DI					; disable interrupts
+
+	CALL	EZ80_INIT
 
 	CALL	UART0_INIT
 	CALL	UART1_INIT
